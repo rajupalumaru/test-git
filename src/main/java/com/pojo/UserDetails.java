cce2d4e2d3e75ba7b8cc7511aa78015a25502907
DELL <DELL@192.168.0.2>
2017-10-31 18:39:17 +0530

    created user pojo and jsp file and added jars

---
+++ src/main/java/com/pojo/UserDetails.java
@@ -0,0 +1,41 @@
+package com.pojo;
+
+public class UserDetails {
+	private Integer id;
+	private String Name;
+	private String Email;
+	private String password;
+	private String MObileno;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return Name;
+	}
+	public void setName(String name) {
+		Name = name;
+	}
+	public String getEmail() {
+		return Email;
+	}
+	public void setEmail(String email) {
+		Email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getMObileno() {
+		return MObileno;
+	}
+	public void setMObileno(String mObileno) {
+		MObileno = mObileno;
+	}
+	
+
+}
