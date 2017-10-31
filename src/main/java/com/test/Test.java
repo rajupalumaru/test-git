ad9e220618b6e6cba377091ac08922bdf936b37e
SAGAR <SAGAR@192.168.0.3>
2017-10-31 19:54:49 +0530

    main method is created

--- src/main/java/com/test/Test.java
+++ src/main/java/com/test/Test.java
@@ -4,7 +4,11 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("test class");
 		System.out.println("once check it ");
-		System.out.println("sagar test");
+		 
 	}
 
+	public void Sagar() {
+		System.out.println("well come to my world");
+		
+	}
 }
